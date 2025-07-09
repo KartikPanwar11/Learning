c09a6f31ff5d5e79b48f271d4f8d1afc0dd15bfd
KartikPanwar11 <Kartikpanwar1101@gmail.com>
2025-07-09 18:51:01 +0530

    JS var let and const

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+// console.log('Hello World');
+
+// const name = 'kartik';
+
+// let name= 'KP';
+// name = 'PK'
+// console.log(name);
+// var name = 'Kp'
+// var name = 'PK'
+// console.log(name) 
+
+
