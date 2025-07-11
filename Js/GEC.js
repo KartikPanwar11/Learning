8123bb5cb06bf40d7e9827c99d594289c60df1cf
KartikPanwar11 <Kartikpanwar1101@gmail.com>
2025-07-11 16:58:20 +0530

    Till Hoisting

---
+++ Js/GEC.js
@@ -0,0 +1,32 @@
+// console.log('Hello World');
+
+// const name = 'kartik';
+
+// let name= 'KP';
+// name = 'PK'
+// console.log(name);
+// var name = 'Kp'
+// var name = 'PK'
+// console.log(name) 
+
+// var a=10
+
+// function a(){
+//     var b=20;
+//     console.log(b);
+
+//     function b(){
+//         var c=30;
+//         console.log(c);
+
+//         function c(){
+//             var c=60;
+//             console.log(c);
+//         }
+//         b();
+//     }
+//     c();
+// }
+// a();
+// console.log(a);
+
---
+++ Js/Hoisting&TDZ.js
@@ -0,0 +1,4 @@
+console.log(a);
+var a = 10 //Undefined
+let a = 10; //Reference error in terms of const too
+const a = 10;
