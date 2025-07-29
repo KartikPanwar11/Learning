75455645ad95ee9fc01b4ae49e9b3245b1562e9a
KartikPanwar11 <Kartikpanwar1101@gmail.com>
2025-07-29 22:12:10 +0530

    Loops half way

--- Worksheet/loops.js
+++ Worksheet/loops.js
@@ -1,5 +1,14 @@
 // Multiplication table
-let num = 2;
-for(let i=0;i<=10;i+num){
-    console.log(i)
-}
+// let num = 2;
+// for(let i=1;i<=10;i++){
+//     console.log(i*num)
+// }
+
+// let fact = 1;
+// for(let i=1;i<=5;i++){
+//     fact *= i
+//     console.log(fact);
+// }
+// console.log(fact);
+
+
