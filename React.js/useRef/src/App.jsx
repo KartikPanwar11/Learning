c95cd9cdaa9e1e0a4be621bb942ddeeef4bce4bb
KartikPanwar11 <Kartikpanwar1101@gmail.com>
2025-08-20 13:25:23 +0530

    UseRef

---
+++ React.js/useRef/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState,useRef } from 'react'
+
+const App = () => {
+  let [count,setCount]= useState(0);
+  
+  let ref = useRef(0);
+  const refe2 = useRef();
+  refe2.current.style.color = 'blue';
+  //dom manipulation using useRef
+
+
+  let inc = ()=>{
+    setCount(count+1);
+    ref.current = ref.current+1;
+    console.log(ref.current)
+
+  
+  }
+  return (
+    
+    <div>
+      <button onClick={inc}>
+        Count is {count}
+      </button>
+      <p ref={refe2}>Changing the color using ref</p>
+    </div>
+  )
+}
+
+export default App
